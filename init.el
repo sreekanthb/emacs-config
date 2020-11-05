d9cb2f61fd73e243bd631fe1f1fac7b799bd5db4
Sreekanthreddy bandi <sreek.4075@gmail.com>
2020-11-05 13:59:11 +0530

    update packages if not there, now loads faster!

--- init.el
+++ init.el
@@ -3,14 +3,23 @@
 ;;; Commentary:
 
 ;;; Code:
-
-
 ;; Intialize packahes
-(require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")))
-(when (not package-archive-contents)
-  (package-refresh-contents t))
+(eval-when-compile
+  (require 'package)
+  (unless (assoc-default "elpa" package-archives)
+    (add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t))
+  (unless (assoc-default "melpa" package-archives)
+    (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+  (unless (assoc-default "org" package-archives)
+    (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t))
+  (package-initialize)
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+  (require 'use-package)
+  (setq use-package-verbose t
+        use-package-always-defer t
+	use-package-always-ensure t))
 
 ;; cool packages to have
 (defvar package-list
